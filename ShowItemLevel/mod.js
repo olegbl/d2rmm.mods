53882d4dbb87e79f743022dd506ce1b223aa53a5
Oleg Lokhvitsky <olegbl@gmail.com>
2022-02-04 10:40:53 -0800

    Add ShowItemLevel mod

---
+++ ShowItemLevel/mod.js
@@ -0,0 +1,28 @@
+if (config.weapons) {
+  const miscFilename = 'global\\excel\\weapons.txt';
+  const misc = D2RMM.readTsv(miscFilename);
+  misc.rows.forEach((row) => {
+    row.ShowLevel = 1;
+  });
+  D2RMM.writeTsv(miscFilename, misc);
+}
+
+if (config.armor) {
+  const miscFilename = 'global\\excel\\armor.txt';
+  const misc = D2RMM.readTsv(miscFilename);
+  misc.rows.forEach((row) => {
+    row.ShowLevel = 1;
+  });
+  D2RMM.writeTsv(miscFilename, misc);
+}
+
+if (config.jewelry) {
+  const miscFilename = 'global\\excel\\misc.txt';
+  const misc = D2RMM.readTsv(miscFilename);
+  misc.rows.forEach((row) => {
+    if (row.code === 'amu' || row.code === 'rin') {
+      row.ShowLevel = 1;
+    }
+  });
+  D2RMM.writeTsv(miscFilename, misc);
+}

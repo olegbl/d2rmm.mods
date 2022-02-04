9d0eef412d4b0e39ddee4fd0af0194a136969214
Oleg Lokhvitsky <olegbl@gmail.com>
2022-02-04 12:10:20 -0800

    Add ShortPotionNames mod

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     'guard-for-in': 'off', // not working with non-pojos
     'no-console': 'error', // console is not available in scope anyway
+    'no-nested-ternary': 'off', // no.
     'no-restricted-syntax': 'off', // we need to iterate using loops since Object is not available in scope
     'no-undef': 'off', // we have global definitions coming from mod manager
     'prefer-destructuring': 'off', // no need to force it here
---
+++ ShortPotionNames/mod.js
@@ -0,0 +1,76 @@
+const COLOR_PREFIX = 'ÿc';
+const COLOR_RED = `${COLOR_PREFIX}1`;
+const COLOR_GREEN = `${COLOR_PREFIX}2`;
+const COLOR_BLUE = `${COLOR_PREFIX}3`;
+const COLOR_GRAY = `${COLOR_PREFIX}5`;
+const COLOR_ORANGE = `${COLOR_PREFIX}8`;
+const COLOR_YELLOW = `${COLOR_PREFIX}9`;
+const COLOR_PURPLE = `${COLOR_PREFIX};`;
+
+const itemNamesFilename = 'local\\lng\\strings\\item-names.json';
+const itemNames = D2RMM.readJson(itemNamesFilename);
+itemNames.forEach((itemName) => {
+  const itemtype = itemName.Key;
+  let newName = null;
+
+  // health
+  if (['hp1', 'hp2', 'hp3', 'hp4', 'hp5'].indexOf(itemtype) !== -1) {
+    newName = `${COLOR_RED}${itemtype.toUpperCase()}`;
+  }
+
+  // mana
+  if (['mp1', 'mp2', 'mp3', 'mp4', 'mp5'].indexOf(itemtype) !== -1) {
+    newName = `${COLOR_BLUE}${itemtype.toUpperCase()}`;
+  }
+
+  // rejuvenation
+  if (['rvl', 'rvs'].indexOf(itemtype) !== -1) {
+    const suffix = itemtype.endsWith('l') ? '2' : '1';
+    newName = `${COLOR_PURPLE}RP${suffix}`;
+  }
+
+  // strangling gas / choking gas / rancid
+  if (['gpl', 'gpm', 'gps'].indexOf(itemtype) !== -1) {
+    const suffix = itemtype.endsWith('l')
+      ? '3'
+      : itemtype.endsWith('m')
+      ? '2'
+      : '1';
+    newName = `${COLOR_GREEN}GP${suffix}`;
+  }
+
+  // fulmigating / exploding / oil
+  if (['opl', 'opm', 'ops'].indexOf(itemtype) !== -1) {
+    const suffix = itemtype.endsWith('l')
+      ? '3'
+      : itemtype.endsWith('m')
+      ? '2'
+      : '1';
+    newName = `${COLOR_ORANGE}EP${suffix}`;
+  }
+
+  // antidote
+  if (itemtype === 'yps') {
+    newName = `${COLOR_GRAY}AP`;
+  }
+
+  // thawing
+  if (itemtype === 'wms') {
+    newName = `${COLOR_YELLOW}TP`;
+  }
+
+  // stamina
+  if (itemtype === 'vps') {
+    newName = `${COLOR_GRAY}SP`;
+  }
+
+  if (newName != null) {
+    // update all localizations
+    for (const key in itemName) {
+      if (key !== 'id' && key !== 'Key') {
+        itemName[key] = newName;
+      }
+    }
+  }
+});
+D2RMM.writeJson(itemNamesFilename, itemNames);

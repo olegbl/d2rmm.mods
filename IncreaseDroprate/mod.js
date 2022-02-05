0d45231251d3f90d613813c47ad7e735090a3f96
Oleg Lokhvitsky <olegbl@gmail.com>
2022-02-05 14:32:15 -0800

    Allow modifying equip droprate in IncreaseDroprate mod

--- IncreaseDroprate/mod.js
+++ IncreaseDroprate/mod.js
@@ -19,3 +19,23 @@ if (config.runes) {
   });
   D2RMM.writeTsv(treasureclassexFilename, treasureclassex);
 }
+
+if (config.equipment) {
+  const { equipmentChance, equipmentMin } = config;
+  const treasureclassexFilename = 'global\\excel\\itemratio.txt';
+  const treasureclassex = D2RMM.readTsv(treasureclassexFilename);
+  treasureclassex.rows.forEach((row) => {
+    row.Unique = Math.max(1, Math.floor(row.Unique / equipmentChance));
+    row.UniqueMin = Math.max(1, Math.floor(row.UniqueMin / equipmentMin));
+
+    row.Set = Math.max(1, Math.floor(row.Set / equipmentChance));
+    row.SetMin = Math.max(1, Math.floor(row.SetMin / equipmentMin));
+
+    row.Rare = Math.max(1, Math.floor(row.Rare / equipmentChance));
+    row.RareMin = Math.max(1, Math.floor(row.RareMin / equipmentMin));
+
+    row.Magic = Math.max(1, Math.floor(row.Magic / equipmentChance));
+    row.MagicMin = Math.max(1, Math.floor(row.MagicMin / equipmentMin));
+  });
+  D2RMM.writeTsv(treasureclassexFilename, treasureclassex);
+}

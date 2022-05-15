7e8f2676036ef032242aaf21b426b9c8e2127de2
Oleg Lokhvitsky <olegbl@gmail.com>
2022-05-14 20:54:02 -0700

    [SkipIntroVideos] Chinese/Japanese localization support

--- SkipIntroVideos/mod.js
+++ SkipIntroVideos/mod.js
@@ -3,3 +3,19 @@ D2RMM.copyFile(
   'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
   true // overwrite any conflicts
 );
+
+if (config.zhtw) {
+  D2RMM.copyFile(
+    'hd', // <mod folder>\hd
+    '..\\locales\\zhtw\\data\\hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+    true // overwrite any conflicts
+  );
+}
+
+if (config.jajp) {
+  D2RMM.copyFile(
+    'hd', // <mod folder>\hd
+    '..\\locales\\jajp\\data\\hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd
+    true // overwrite any conflicts
+  );
+}

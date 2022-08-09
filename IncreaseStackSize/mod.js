1caa54a6d239aeae95a0e7e9cb0df93b9f90a302
Oleg Lokhvitsky <olegbl@gmail.com>
2022-08-09 15:44:09 -0700

    [IncreaseStackSize] Allow customizing stack sizes

--- IncreaseStackSize/mod.js
+++ IncreaseStackSize/mod.js
@@ -1,34 +1,38 @@
-if (config.items) {
-  const miscFilename = 'global\\excel\\misc.txt';
-  const misc = D2RMM.readTsv(miscFilename);
-  misc.rows.forEach((row) => {
-    if (
-      row.code === 'tbk' || // Tome of Town Portal
-      row.code === 'ibk' || // Tome of Identify
-      row.code === 'aqv' || // Arrows
-      row.code === 'cqb' || // Bolts
-      row.code === 'key' // Key
-    ) {
-      row.maxstack = 500;
-    }
-  });
-  D2RMM.writeTsv(miscFilename, misc);
-}
+const miscFilename = 'global\\excel\\misc.txt';
+const misc = D2RMM.readTsv(miscFilename);
+misc.rows.forEach((row) => {
+  if (
+    row.code === 'tbk' || // Tome of Town Portal
+    row.code === 'ibk' // Tome of Identify
+  ) {
+    row.maxstack = config.tomes;
+  }
+  if (
+    row.code === 'aqv' || // Arrows
+    row.code === 'cqb' // Bolts
+  ) {
+    row.maxstack = config.projectiles;
+  }
+  if (
+    row.code === 'key' // Key
+  ) {
+    row.maxstack = config.keys;
+  }
+});
+D2RMM.writeTsv(miscFilename, misc);
 
-if (config.weapons) {
-  const weaponsFilename = 'global\\excel\\weapons.txt';
-  const weapons = D2RMM.readTsv(weaponsFilename);
-  weapons.rows.forEach((row) => {
-    if (
-      // don't modify throwing potions (gas, oil pots)
-      row.type !== 'tpot' &&
-      // only modify throwing weapons
-      row.stackable === '1'
-    ) {
-      row.minstack = 500;
-      row.maxstack = 500;
-      row.spawnstack = 500;
-    }
-  });
-  D2RMM.writeTsv(weaponsFilename, weapons);
-}
+const weaponsFilename = 'global\\excel\\weapons.txt';
+const weapons = D2RMM.readTsv(weaponsFilename);
+weapons.rows.forEach((row) => {
+  if (
+    // don't modify throwing potions (gas, oil pots)
+    row.type !== 'tpot' &&
+    // only modify throwing weapons
+    row.stackable === '1'
+  ) {
+    row.minstack = config.throwingweapons;
+    row.maxstack = config.throwingweapons;
+    row.spawnstack = config.throwingweapons;
+  }
+});
+D2RMM.writeTsv(weaponsFilename, weapons);

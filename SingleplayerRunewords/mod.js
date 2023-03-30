b68d3f3a7481d1c1582a1df6229ddef9a7a939ad
Oleg Lokhvitsky <olegbl@gmail.com>
2023-03-29 18:13:32 -0700

    [SingleplayerRunewords] Update for D2R 2.6

--- SingleplayerRunewords/mod.js
+++ SingleplayerRunewords/mod.js
@@ -3,5 +3,9 @@ const runes = D2RMM.readTsv(runesFilename);
 runes.rows.forEach((row) => {
   // https://d2mods.info/forum/kb/viewarticle?a=388
   row.server = '';
+
+  // version 2.6 got rid of "server" column and replaced it with these:
+  row.firstLadderSeason = '';
+  row.lastLadderSeason = '';
 });
 D2RMM.writeTsv(runesFilename, runes);

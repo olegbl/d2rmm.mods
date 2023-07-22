2b77a5842b3ecd8134e9e61d68c220a4c9fff3ea
Oleg Lokhvitsky <olegbl@gmail.com>
2023-07-22 08:01:41 -0700

    [SkipIntroVideos] Make the mod uninstall properly in -direct mode

--- SkipIntroVideos/mod.js
+++ SkipIntroVideos/mod.js
@@ -1,3 +1,8 @@
+// touching the files makes sure that the mod can
+// be uninstalled properly when using -direct mode
+D2RMM.readTxt('hd\\global\\video\\blizzardlogos.webm');
+D2RMM.readTxt('hd\\global\\video\\logoanim.webm');
+
 D2RMM.copyFile(
   'hd', // <mod folder>\hd
   'hd', // <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd

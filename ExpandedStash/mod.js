7509e91f5271a74e7bd0527d20e9f2868abd1c82
Caedendi <Caedendi@users.noreply.github.com>
2024-03-24 21:18:33 +0100

    Fixed broken checks for settings

--- ExpandedStash/mod.js
+++ ExpandedStash/mod.js
@@ -1,11 +1,11 @@
-const tabNamePersonal = (config.IsCustomTabNamesEnabled && config.tabNamePersonal === "") ? "@personal" : config.tabNamePersonal;
-const tabNameShared1  = (config.IsCustomTabNamesEnabled && config.tabNameShared1  === "") ? "@shared"   : config.tabNameShared1;
-const tabNameShared2  = (config.IsCustomTabNamesEnabled && config.tabNameShared2  === "") ? "@shared"   : config.tabNameShared2;
-const tabNameShared3  = (config.IsCustomTabNamesEnabled && config.tabNameShared3  === "") ? "@shared"   : config.tabNameShared3;
-const tabNameShared4  = (config.IsCustomTabNamesEnabled && config.tabNameShared4  === "") ? "@shared"   : config.tabNameShared4;
-const tabNameShared5  = (config.IsCustomTabNamesEnabled && config.tabNameShared5  === "") ? "@shared"   : config.tabNameShared5;
-const tabNameShared6  = (config.IsCustomTabNamesEnabled && config.tabNameShared6  === "") ? "@shared"   : config.tabNameShared6;
-const tabNameShared7  = (config.IsCustomTabNamesEnabled && config.tabNameShared7  === "") ? "@shared"   : config.tabNameShared7;
+const tabNamePersonal = (!config.IsCustomTabNamesEnabled || config.tabNamePersonal === "") ? "@personal" : config.tabNamePersonal;
+const tabNameShared1  = (!config.IsCustomTabNamesEnabled || config.tabNameShared1  === "") ? "@shared"   : config.tabNameShared1;
+const tabNameShared2  = (!config.IsCustomTabNamesEnabled || config.tabNameShared2  === "") ? "@shared"   : config.tabNameShared2;
+const tabNameShared3  = (!config.IsCustomTabNamesEnabled || config.tabNameShared3  === "") ? "@shared"   : config.tabNameShared3;
+const tabNameShared4  = (!config.IsCustomTabNamesEnabled || config.tabNameShared4  === "") ? "@shared"   : config.tabNameShared4;
+const tabNameShared5  = (!config.IsCustomTabNamesEnabled || config.tabNameShared5  === "") ? "@shared"   : config.tabNameShared5;
+const tabNameShared6  = (!config.IsCustomTabNamesEnabled || config.tabNameShared6  === "") ? "@shared"   : config.tabNameShared6;
+const tabNameShared7  = (!config.IsCustomTabNamesEnabled || config.tabNameShared7  === "") ? "@shared"   : config.tabNameShared7;
 
 const inventoryFilename = 'global\\excel\\inventory.txt';
 const inventory = D2RMM.readTsv(inventoryFilename);

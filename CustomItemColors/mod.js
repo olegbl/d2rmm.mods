d3b27b8cc2142285c1ebf057cadf3a14d4136c00
Caedendi <Caedendi@users.noreply.github.com>
2024-03-25 03:48:40 +0100

    first draft

--- CustomItemColors/mod.js
+++ CustomItemColors/mod.js
@@ -1,68 +1,75 @@
-const COLORS = {
-  Beige: '$FontColorBeige',
-  Black: '$FontColorBlack',
-  Blue: '$FontColorBlue',
-  CurrencyGold: '$FontColorCurrencyGold',
-  DarkGold: '$FontColorDarkGold',
-  DarkGrayBlue: '$FontColorDarkGrayBlue',
-  DarkGrayGold: '$FontColorDarkGrayGold',
-  DarkGreen: '$FontColorDarkGreen',
-  Gold: '$FontColorGold',
-  GoldYellow: '$FontColorGoldYellow',
-  Gray: '$FontColorGray',
-  Green: '$FontColorGreen',
-  LightBlue: '$FontColorLightBlue',
-  LightGold: '$FontColorLightGold',
-  LightGray: '$FontColorLightGray',
-  LightPurple: '$FontColorLightPurple',
-  LightRed: '$FontColorLightRed',
-  LightTeal: '$FontColorLightTeal',
-  LightYellow: '$FontColorLightYellow',
-  Orange: '$FontColorOrange',
-  PartyGreen: '$FontColorPartyGreen',
-  PartyOrange: '$FontColorPartyOrange',
-  Red: '$FontColorRed',
-  Transparent: '$FontColorTransparent',
-  VeryLightGray: '$FontColorVeryLightGray',
-  White: '$FontColorWhite',
-  Yellow: '$FontColorYellow',
-};
+class FileConstants {
+  static EXTENSION_JSON = ".json";
+  static PATH_GLOBAL_UI_LAYOUTS = `global\\ui\\layouts\\`;
 
+  static FILE_PROFILE_HD            = `${this.PATH_GLOBAL_UI_LAYOUTS}_profilehd${this.EXTENSION_JSON}`;
+  static FILE_PROFILE_LV            = `${this.PATH_GLOBAL_UI_LAYOUTS}_profilelv${this.EXTENSION_JSON}`;
+  static FILE_CONTROLLER_PROFILE_LV = `${this.PATH_GLOBAL_UI_LAYOUTS}controller\\_profilelv${this.EXTENSION_JSON}`;
+}
+
+class Colors {
+  static prefix = "$FontColor";
+  
+  static Beige         = `${this.prefix}Beige`;
+  static Black         = `${this.prefix}Black`;
+  static Blue          = `${this.prefix}Blue`;
+  static CurrencyGold  = `${this.prefix}CurrencyGold`;
+  static DarkGold      = `${this.prefix}DarkGold`;
+  static DarkGrayBlue  = `${this.prefix}DarkGrayBlue`;
+  static DarkGrayGold  = `${this.prefix}DarkGrayGold`;
+  static DarkGreen     = `${this.prefix}DarkGreen`;
+  static Gold          = `${this.prefix}Gold`;
+  static GoldYellow    = `${this.prefix}GoldYellow`;
+  static Gray          = `${this.prefix}Gray`;
+  static Green         = `${this.prefix}Green`;
+  static LightBlue     = `${this.prefix}LightBlue`;
+  static LightGold     = `${this.prefix}LightGold`;
+  static LightGray     = `${this.prefix}LightGray`;
+  static LightPurple   = `${this.prefix}LightPurple`;
+  static LightRed      = `${this.prefix}LightRed`;
+  static LightTeal     = `${this.prefix}LightTeal`;
+  static LightYellow   = `${this.prefix}LightYellow`;
+  static Orange        = `${this.prefix}Orange`;
+  static PartyGreen    = `${this.prefix}PartyGreen`;
+  static PartyOrange   = `${this.prefix}PartyOrange`;
+  static Red           = `${this.prefix}Red`;
+  static Transparent   = `${this.prefix}Transparent`;
+  static VeryLightGray = `${this.prefix}VeryLightGray`;
+  static White         = `${this.prefix}White`;
+  static Yellow        = `${this.prefix}Yellow`;
+}
+
+const color = "Color";
 const VARIABLES = [
-  'DefaultColor',
-  'EtherealColor',
-  'SocketedColor',
-  'MagicColor',
-  'RareColor',
-  'SetColor',
-  'UniqueColor',
-  'CraftedColor',
-  'TemperedColor',
-  'QuestColor',
-  'GoldColor',
+  `Default${color}`,
+  `Ethereal${color}`,
+  `Socketed${color}`,
+  `Magic${color}`,
+  `Rare${color}`,
+  `Set${color}`,
+  `Unique${color}`,
+  `Crafted${color}`,
+  `Tempered${color}`,
+  `Quest${color}`,
+  `Gold${color}`,
 ];
 
-function changeProfileColors(profile) {
+function setColorsForFile(path) {
+  let file = D2RMM.readJson(path);
+  setProfileColors(file);
+  D2RMM.writeJson(path, file);
+}
+
+// todo: skip setting values if set to default, OR add a "disable" setting while clarifying that the default value will force-overwrite.
+function setProfileColors(file) {
   VARIABLES.forEach((variable) => {
-    const color = COLORS[config[variable]];
+    let color = Colors[config[variable]];
     if (color != null) {
-      profile.TooltipStyle[variable] = color;
+      file.TooltipStyle[variable] = color;
     }
   });
 }
 
-const profileHDFilename = 'global\\ui\\layouts\\_profilehd.json';
-const profileHD = D2RMM.readJson(profileHDFilename);
-changeProfileColors(profileHD);
-D2RMM.writeJson(profileHDFilename, profileHD);
-
-const profileLVFilename = 'global\\ui\\layouts\\_profilelv.json';
-const profileLV = D2RMM.readJson(profileLVFilename);
-changeProfileColors(profileLV);
-D2RMM.writeJson(profileLVFilename, profileLV);
-
-const controllerProfileLVFilename =
-  'global\\ui\\layouts\\controller\\_profilelv.json';
-const controllerProfileLV = D2RMM.readJson(controllerProfileLVFilename);
-changeProfileColors(controllerProfileLV);
-D2RMM.writeJson(controllerProfileLVFilename, controllerProfileLV);
+setColorsForFile(FILE_PROFILE_HD);
+setColorsForFile(FILE_PROFILE_LV);
+setColorsForFile(FILE_CONTROLLER_PROFILE_LV);

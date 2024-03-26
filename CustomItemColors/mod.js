2c512625587ec6626f752060606be159c76e0556
Caedendi <Caedendi@users.noreply.github.com>
2024-03-26 01:05:21 +0100

    Finished

--- CustomItemColors/mod.js
+++ CustomItemColors/mod.js
@@ -7,69 +7,51 @@ class FileConstants {
   static FILE_CONTROLLER_PROFILE_LV = `${this.PATH_GLOBAL_UI_LAYOUTS}controller\\_profilelv${this.EXTENSION_JSON}`;
 }
 
-class Colors {
-  static prefix = "$FontColor";
+class ColorConstants {
+  static fontColorPrefix = "$FontColor";
+  static colorSuffix = "Color";
+  static fontColors = [
+    `Default${this.colorSuffix}`,
+    `Ethereal${this.colorSuffix}`,
+    `Socketed${this.colorSuffix}`,
+    `Magic${this.colorSuffix}`,
+    `Rare${this.colorSuffix}`,
+    `Set${this.colorSuffix}`,
+    `Unique${this.colorSuffix}`,
+    `Crafted${this.colorSuffix}`,
+    `Tempered${this.colorSuffix}`,
+    `Quest${this.colorSuffix}`,
+    `Gold${this.colorSuffix}`,
+    `HealthPotion${this.colorSuffix}`,
+    `ManaPotion${this.colorSuffix}`,
+    `RejuvPotion${this.colorSuffix}`,
+    `Rune${this.colorSuffix}`,
+    `EventItems${this.colorSuffix}`,
+  ];
+}
+
+class CustomItemColorsMod {
+  apply() {
+    this.setColorsForFile(FileConstants.FILE_PROFILE_HD);
+    this.setColorsForFile(FileConstants.FILE_PROFILE_LV);
+    this.setColorsForFile(FileConstants.FILE_CONTROLLER_PROFILE_LV);
+  }
+
+  setColorsForFile(path) {
+    let file = D2RMM.readJson(path);
+    this.setProfileColors(file);
+    D2RMM.writeJson(path, file);
+  }
   
-  static Beige         = `${this.prefix}Beige`;
-  static Black         = `${this.prefix}Black`;
-  static Blue          = `${this.prefix}Blue`;
-  static CurrencyGold  = `${this.prefix}CurrencyGold`;
-  static DarkGold      = `${this.prefix}DarkGold`;
-  static DarkGrayBlue  = `${this.prefix}DarkGrayBlue`;
-  static DarkGrayGold  = `${this.prefix}DarkGrayGold`;
-  static DarkGreen     = `${this.prefix}DarkGreen`;
-  static Gold          = `${this.prefix}Gold`;
-  static GoldYellow    = `${this.prefix}GoldYellow`;
-  static Gray          = `${this.prefix}Gray`;
-  static Green         = `${this.prefix}Green`;
-  static LightBlue     = `${this.prefix}LightBlue`;
-  static LightGold     = `${this.prefix}LightGold`;
-  static LightGray     = `${this.prefix}LightGray`;
-  static LightPurple   = `${this.prefix}LightPurple`;
-  static LightRed      = `${this.prefix}LightRed`;
-  static LightTeal     = `${this.prefix}LightTeal`;
-  static LightYellow   = `${this.prefix}LightYellow`;
-  static Orange        = `${this.prefix}Orange`;
-  static PartyGreen    = `${this.prefix}PartyGreen`;
-  static PartyOrange   = `${this.prefix}PartyOrange`;
-  static Red           = `${this.prefix}Red`;
-  static Transparent   = `${this.prefix}Transparent`;
-  static VeryLightGray = `${this.prefix}VeryLightGray`;
-  static White         = `${this.prefix}White`;
-  static Yellow        = `${this.prefix}Yellow`;
+  setProfileColors(file) {
+    ColorConstants.fontColors.forEach(fontColor => {
+      let newColor = config[fontColor];
+      if (newColor == null || newColor === "disable") {
+        return;
+      }
+      file.TooltipStyle[fontColor] = `${ColorConstants.fontColorPrefix}${newColor}`;
+    });
+  }
 }
 
-const color = "Color";
-const VARIABLES = [
-  `Default${color}`,
-  `Ethereal${color}`,
-  `Socketed${color}`,
-  `Magic${color}`,
-  `Rare${color}`,
-  `Set${color}`,
-  `Unique${color}`,
-  `Crafted${color}`,
-  `Tempered${color}`,
-  `Quest${color}`,
-  `Gold${color}`,
-];
-
-function setColorsForFile(path) {
-  let file = D2RMM.readJson(path);
-  setProfileColors(file);
-  D2RMM.writeJson(path, file);
-}
-
-// todo: skip setting values if set to default, OR add a "disable" setting while clarifying that the default value will force-overwrite.
-function setProfileColors(file) {
-  VARIABLES.forEach((variable) => {
-    let color = Colors[config[variable]];
-    if (color != null) {
-      file.TooltipStyle[variable] = color;
-    }
-  });
-}
-
-setColorsForFile(FILE_PROFILE_HD);
-setColorsForFile(FILE_PROFILE_LV);
-setColorsForFile(FILE_CONTROLLER_PROFILE_LV);
+(new CustomItemColorsMod()).apply();

99a6b8c9332a8bac673a3c28c60ad2ba74efe7e1
Caedendi <Caedendi@users.noreply.github.com>
2024-03-26 01:55:39 +0100

    Fixed description for Keys setting

--- IncreaseStackSize/mod.js
+++ IncreaseStackSize/mod.js
@@ -3,13 +3,13 @@ const misc = D2RMM.readTsv(miscFilename);
 misc.rows.forEach((row) => {
   if (
     row.code === 'tbk' || // Tome of Town Portal
-    row.code === 'ibk' // Tome of Identify
+    row.code === 'ibk'    // Tome of Identify
   ) {
     row.maxstack = config.tomes;
   }
   if (
     row.code === 'aqv' || // Arrows
-    row.code === 'cqv' // Bolts
+    row.code === 'cqv'    // Bolts
   ) {
     row.maxstack = config.projectiles;
   }
@@ -25,10 +25,8 @@ const weaponsFilename = 'global\\excel\\weapons.txt';
 const weapons = D2RMM.readTsv(weaponsFilename);
 weapons.rows.forEach((row) => {
   if (
-    // don't modify throwing potions (gas, oil pots)
-    row.type !== 'tpot' &&
-    // only modify throwing weapons
-    row.stackable === '1'
+    row.type !== 'tpot' && // don't modify throwing potions (gas, oil pots)
+    row.stackable === '1'  // only modify throwing weapons
   ) {
     row.minstack = config.throwingweapons;
     row.maxstack = config.throwingweapons;
